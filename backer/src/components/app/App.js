d97fabe34f5a9a4e80819847954293fecdc63b66
Benjamin Rodriguez <bimatn@outlook.com>
2017-12-09 13:49:06 -0700

    navbar react work

--- backer/src/components/app/App.js
+++ backer/src/components/app/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import NavBar from '../bootstrap/navbar.js';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <NavBar navName="Potato" testNum={5+7}/>
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
