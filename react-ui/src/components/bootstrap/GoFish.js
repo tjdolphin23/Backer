5e0938a438ca1e5a2a9371f9a238036fb1c4f8c3
Benjamin Rodriguez <bimatn@outlook.com>
2017-12-18 11:40:19 -0700

    updated site name and added tagline

--- react-ui/src/components/bootstrap/GoFish.js
+++ react-ui/src/components/bootstrap/GoFish.js
@@ -1,9 +1,16 @@
 import React from 'react';
 
 const goFish = () =>
-	<div className="row justify-content-center">
-		<div className="text-center col-4  GoFish">
-			<p>GO FISH</p>
+	<div>
+		<div className="row justify-content-center">
+			<div className="text-center col-4">
+				<p className="GoFish">GO FISH</p>
+			</div>
+		</div>
+		<div className="row justify-content-center">
+			<div className="text-center col-6">
+				<h4>Where Entrepeneurs Come to Cast Their Lines</h4>
+			</div>
 		</div>
 	</div>
 
