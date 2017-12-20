6cbdabde3b45e3094b914adc3d1e660e9480b497
Tom Dolphin <thedolphingroup1@gmail.com>
2017-12-20 15:00:37 -0700

    created models folder & created connection.js to  mysql

---
+++ server/controllers/connection.js
@@ -0,0 +1,17 @@
+// *********************************************************************************
+// CONNECTION.JS - THIS FILE INITIATES THE CONNECTION TO MYSQL
+// *********************************************************************************
+// Dependencies
+var Sequelize = require("sequelize");
+// Creates mySQL connection using Sequelize
+var sequelize = new Sequelize("backer", "s4o6h6fg2ixdyxen", "aorynnkw0e9ps36k", {
+  host: "tk3mehkfmmrhjg0b.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+  dialect: "mysql",
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000
+  }
+});
+// Exports the connection for other files to use
+module.exports = sequelize;
