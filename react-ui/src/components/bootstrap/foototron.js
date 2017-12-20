f409f2c77ddc2d6b01a5551744342abc44783fa6
Benjamin Rodriguez <bimatn@outlook.com>
2017-12-19 17:48:34 -0700

    changing col to col-lg

--- react-ui/src/components/bootstrap/foototron.js
+++ react-ui/src/components/bootstrap/foototron.js
@@ -4,24 +4,24 @@ const Foot = () =>
 
 <div className="foot">
 	<div className="row justify-content-center text-center">
-		<div className="col-6">
+		<div className="col-lg-6">
 			<div className="row">
-				<div className="col-2">
+				<div className="col-lg-2">
 					<a href="#" alt="Philosophy"><h5 className="footLinks">Philosophy</h5></a>
 				</div>
-				<div className="col-2">
+				<div className="col-lg-2">
 					<a href="#" alt="Philosophy"><h5 className="footLinks">About Us</h5></a>
 				</div>
-				<div className="col-2">
+				<div className="col-lg-2">
 					<a href="#" alt="Philosophy"><h5 className="footLinks">API</h5></a>
 				</div>
-				<div className="col-2">
+				<div className="col-lg-2">
 					<a href="#" alt="Philosophy"><h5 className="footLinks">Careers</h5></a>
 				</div>
-				<div className="col-2">
+				<div className="col-lg-2">
 					<a href="#" alt="Philosophy"><h5 className="footLinks">Contact</h5></a>
 				</div>
-				<div className="col-2">
+				<div className="col-lg-2">
 					<a href="#" alt="Philosophy"><h5 className="footLinks">Legal</h5></a>
 				</div>
 			</div>
