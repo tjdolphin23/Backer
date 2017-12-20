07f4af2f6a93badfe9418af1c492fa55f1a2ae24
Benjamin Rodriguez <bimatn@outlook.com>
2017-12-20 12:46:47 -0700

    Removed public facing css sheet
    Added the styling to the app.css file

---
+++ react-ui/src/pages/founders.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import NavBar from '../components/bootstrap/navbar.js';
+import Container from '../components/bootstrap/container.js';
+import ContainerFluid from '../components/bootstrap/containerFluid.js';
+import Testimonials from '../components/bootstrap/testimonials.js';
+import GoFish from '../components/bootstrap/GoFish.js';
+import Jumbotron from '../components/bootstrap/jumbotron.js';
+import How from '../components/bootstrap/howItWorks.js';
+import Foot from '../components/bootstrap/foototron.js';
+
+const Home = () =>
+
+<div className="App">
+	<Jumbotron>
+		    <NavBar/>
+		    <GoFish/>
+	</Jumbotron>
+	<Container>
+		<How/>
+		<Testimonials/>
+	</Container>
+	<Foot/>
+</div>
+
+export default Home;
