74bc4b57416c922bf1b7dcc1215607303e0ec4b7
Benjamin Rodriguez <bimatn@outlook.com>
2017-12-28 13:37:38 -0700

    Updated Navbar to be a flexible component

--- react-ui/src/components/bootstrap/navbar.js
+++ react-ui/src/components/bootstrap/navbar.js
@@ -1,16 +1,16 @@
 import React from 'react';
 
-const Navbar = () =>
+const Navbar = ({t1, l1, t2, l2, t3, l3, t4, l4}) =>
 	<div className="row justify-content-center">
 		<div className="col-10">
 			<div className="row">
 				<div className="col-xs-12 col-lg-4 mt-4">
 					<ul className="nav">
 					  <li className="nav-item mx-auto">
-					    <a className="nav-link" href="/founders"><h4>Founders</h4></a>
+					    <a className="nav-link" href={l1}><h4>{t1}</h4></a>
 					  </li>
 					  <li className="nav-item mx-auto">
-					    <a className="nav-link" href="/investors"><h4>Angels</h4></a>
+					    <a className="nav-link" href={l2}><h4>{t2}</h4></a>
 					  </li>
 					</ul>
 				</div>
@@ -20,10 +20,10 @@ const Navbar = () =>
 				<div className="col-xs-12 col-lg-4 mt-4">
 					<ul className="nav">
 					  <li className="nav-item mx-auto">
-					    <a className="nav-link" href="/investors"><h4>Sign In</h4></a>
+					    <a className="nav-link" href={l3}><h4>{t3}</h4></a>
 					  </li>
 					  <li className="nav-item mx-auto">
-					    <a className="nav-link" href="/signin"><h4>Sign Up</h4></a>
+					    <a className="nav-link" href={l4}><h4>{t4}</h4></a>
 					  </li>
 					</ul>
 				</div>
