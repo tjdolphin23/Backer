788f5bcecafb00512c6b29e39a9ed41d4ce5c4e5
Benjamin Rodriguez <bimatn@outlook.com>
2017-12-28 15:28:42 -0700

    Toying around with response of navbar

--- react-ui/src/components/bootstrap/navbar.js
+++ react-ui/src/components/bootstrap/navbar.js
@@ -4,7 +4,7 @@ const Navbar = ({t1, l1, t2, l2, t3, l3, t4, l4}) =>
 	<div className="row justify-content-center">
 		<div className="col-10">
 			<div className="row justify-content-center">
-				<div className="col-xs-12 col-lg-4 mt-4">
+				<div className="col-xs-12 col-md-4 mt-4">
 					<ul className="nav">
 					  <li className="nav-item mx-auto">
 					    <a className="nav-link" href={l1}><h4>{t1}</h4></a>
@@ -14,10 +14,10 @@ const Navbar = ({t1, l1, t2, l2, t3, l3, t4, l4}) =>
 					  </li>
 					</ul>
 				</div>
-				<div className="col-xs-12 col-lg-3 text-center">
-				    <img src="assets/images/angelFish.png" className="w-25 mt-1" alt="Angel Fish Logo"></img>
+				<div className="col-xs-4 col-md-3 text-center">
+				    <img src="assets/images/angelFish.png" className="img-fluid mt-1 w-25" alt="Angel Fish Logo"></img>
 				</div>
-				<div className="col-xs-12 col-lg-4 mt-4">
+				<div className="col-xs-12 col-md-4 mt-4">
 					<ul className="nav">
 					  <li className="nav-item mx-auto">
 					    <a className="nav-link" href={l3}><h4>{t3}</h4></a>
--- react-ui/src/pages/home.js
+++ react-ui/src/pages/home.js
@@ -14,7 +14,7 @@ const Home = () =>
 <div>
 	<JumbotronFluid css="head">
 		<ContainerFluid>
-			    <NavBar t1="Founders" l1="/founders" t2="Angels" l2="/angels" t3="About" l3="/about" t4="Sign In | Register" l4="/getIn"/>
+			    <NavBar t1="Founders" l1="/founders" t2="Angels" l2="/angels" t3="About" l3="/about" t4="Start Now" l4="/getIn"/>
 			    <Title text1="ANGEL FISH" text2="Where Entrepenuers Go To Cast Their Lines"/>
 		</ContainerFluid>
 	</JumbotronFluid>
