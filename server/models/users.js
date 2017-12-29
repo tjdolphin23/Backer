39ed99254fe193b5e80806914aea4d5160c4c636
Benjamin Rodriguez <bimatn@outlook.com>
2017-12-29 12:23:08 -0700

    Creating and modding models

--- server/models/users.js
+++ server/models/users.js
@@ -1,4 +1,35 @@
-// Burger models
+// Creates a "user" model that matches up with DB
+var User = sequelize.define("users", {
+    // id of user (int)
+    id: Sequelize.INTEGER,
+    // name of users (a string)
+    name: Sequelize.STRING,
+    // the user's username (a string)
+    username: Sequelize.STRING,
+    // user's email address (a string)
+    email: Sequelize.STRING,
+    // user's password (a string)
+    password: Sequelize.STRING,
+    // is a founder (a boolean)
+    founder: Sequelize.BOOLEAN,
+    // is a isUser (a boolean)
+    isUser: Sequelize.BOOLEAN,
+    // is a investor (a boolean)
+    investor: Sequelize.BOOLEAN,
+    // user's country location (a string)
+    country: Sequelize.STRING,
+  });
+  // Syncs with DB
+  User.sync();
+  // Makes the user Model available for other files (will also create a table)
+  module.exports = User;
+
+
+
+
+
+/*-------------------------------------------The below code is for guidance--------------------------------------------------*/
+/*// Burger models
 
 // The burger has a burger_name attribute of type DataTypes.String
 // and a devoured attribute that is false by default
@@ -21,4 +52,4 @@ module.exports = function(sequelize, DataTypes) {
     }
   });
   return Burger;
-};
+};*/
