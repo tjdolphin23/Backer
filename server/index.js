2afa0594119eae407e80bf85529ed8e76201ed57
Benjamin Rodriguez <bimatn@outlook.com>
2017-12-29 11:43:58 -0700

    Modifying index.js file for server

--- server/index.js
+++ server/index.js
@@ -2,11 +2,30 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 5000;
-
+const bodyParser = require("body-parser");
+// bring in the models
+const db = require(path.resolve(__dirname, "./models"));
+// API routes
+const routes = require(path.resolve(__dirname, "./controllers/angelFish_Controller"));
 // Priority serve any static files.
 app.use(express.static(path.resolve(__dirname, '../react-ui/build')));
+// override with POST having ?_method=DELETE
+app.use(methodOverride("_method"));
 
-/*------------API ROUTES and Requires-----------------*/
+/*-----------------------------------React Routes--------------------------------------*/
+// All remaining requests return the React app, so it can handle routing.
+app.get('*', function(request, response) {
+  response.sendFile(path.resolve(__dirname, '../Build/react-ui', 'index.html'));
+});
+/*------------------------Sync DB and Start Express Server-----------------------------*/
+db.sequelize.sync().then(function() {
+  app.listen(PORT, function () {
+    console.log(`Listening on port ${PORT}`);
+  });
+});
+
+//-----------------------------------------------------------------------------------------------------------
+/*------------API ROUTES and Requires-----------------
 const Users = require((path.resolve(__dirname, "./models/backers.js")));
 // Answer API requests.
 app.get('/api', function (req, res) {
@@ -25,14 +44,10 @@ app.get('/api/about', function (req, res) {
   Users.findAll({}).then(function(results) {
     res.json(results);
   });
-});
-
-/*------------React Routes-----------------------------*/
-// All remaining requests return the React app, so it can handle routing.
-app.get('*', function(request, response) {
-  response.sendFile(path.resolve(__dirname, '../Build/react-ui', 'index.html'));
-});
-
-app.listen(PORT, function () {
-  console.log(`Listening on port ${PORT}`);
-});
+});*/
+//------------------------------------------------------------------------------------------------------------
+/*// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({
+  extended: false
+}));*/
+//------------------------------------------------------------------------------------------------------------
