6d0da48357a1341837f5b820ae711ece43b11a19
Benjamin Rodriguez <bimatn@outlook.com>
2017-12-31 13:28:16 -0700

    Form for register works
    Now just to write request to backend

--- react-ui/src/components/bootstrap/register.js
+++ react-ui/src/components/bootstrap/register.js
@@ -4,16 +4,22 @@ class Register extends Component {
 	// Setting the component's initial state
 	state = {
 	  firstName: "",
-	  lastName: ""
+	  lastName: "",
+	  userName: "",
+	  email: "",
+	  password: "",
+	  country: "",
+	  founder: false,
+	  angel: false
 	};
 
 	handleInputChange = event => {
 	  // Getting the value and name of the input which triggered the change
-	  const { name, value } = event.target;
+	  const { id, value } = event.target;
 
 	  // Updating the input's state
 	  this.setState({
-	    [name]: value
+	    [id]: value
 	  });
 	};
 
@@ -22,10 +28,20 @@ class Register extends Component {
 	  event.preventDefault();
 
 	  // Alert the user their first and last name, clear `this.state.firstName` and `this.state.lastName`, clearing the inputs
-	  alert(`Hello ${this.state.firstName} ${this.state.lastName}`);
+	  alert(`Hello ${this.state.firstName} ${this.state.lastName} here are your stats:
+	  	${this.state.userName}
+	  	${this.state.email}
+	  	${this.state.password}
+	  	${this.state.country}
+	  	${this.state.role}`);
 	  this.setState({
 	    firstName: "",
-	    lastName: ""
+	    lastName: "",
+	    userName: "",
+	    email: "",
+	    password: "",
+	    country: "",
+	    role: ""
 	  });
 	};
 	//RenderTime
@@ -43,29 +59,30 @@ class Register extends Component {
 						<form>
 							<div className="form-group">
 							  <label htmlFor="firstName">First Name</label>
-							  <textarea className="form-control" id="firstName" rows="1"></textarea>
+							  <textarea className="form-control" id="firstName" value={this.state.firstName} onChange={this.handleInputChange} rows="1"></textarea>
 							</div>
 							<div className="form-group">
 							  <label htmlFor="lastName">Last Name</label>
-							  <textarea className="form-control" id="lastName" rows="1"></textarea>
+							  <textarea className="form-control" id="lastName" value={this.state.lastName} onChange={this.handleInputChange} rows="1"></textarea>
 							</div>
 							<div className="form-group">
 							  <label htmlFor="userName">User Name</label>
-							  <textarea className="form-control" id="userName" rows="1"></textarea>
+							  <textarea className="form-control" id="userName" value={this.state.userName} onChange={this.handleInputChange} rows="1"></textarea>
 							  <small id="userHelp" className="form-text text-muted">This is the name others will see</small>
 							</div>
 							<div className="form-group">
 								<label htmlFor="registerEmail">Email address</label>
-								<input type="email" className="form-control" id="registerEmail" aria-describedby="emailHelp" placeholder="Enter email"></input>
+								<input type="email" className="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter email" value={this.state.email} onChange={this.handleInputChange}></input>
 								<small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
 							</div>
 							<div className="form-group">
 								<label htmlFor="registerPassword">Password</label>
-								<input type="password" className="form-control" id="registerPassword" placeholder="Create Password"></input>
+								<input type="password" className="form-control" id="password" placeholder="Create Password" value={this.state.password} onChange={this.handleInputChange}></input>
 							</div>
 							<div className="form-group">
 							  <label htmlFor="country">Country</label>
-							  <select className="form-control" id="countrySelect">
+							  <select className="form-control" id="country" value={this.state.country} onChange={this.handleInputChange}>
+							  	<option></option>
 							  	<option>Australia</option>
 							  	<option>Canada</option>
 							  	<option>Netherlands</option>
@@ -73,20 +90,16 @@ class Register extends Component {
 							    <option>United States</option>					    
 							  </select>
 							</div>
-							<div className="row justify-content-center">
-								<div className="col-12 text-center">
-									<p>Select A Role</p>
-								</div>
-								<div className="form-check col-5 text-center">
-									<input type="checkbox" className="form-check-input" id="founder"></input>
-									<label className="form-check-label" htmlFor="founder">I'm A Founder</label>
-								</div>
-								<div className="form-check col-5 text-center">
-									<input type="checkbox" className="form-check-input" id="angel"></input>
-									<label className="form-check-label" htmlFor="angel">I'm An Angel</label>
-								</div>
+							<div className="form-group">
+							  <label htmlFor="role">Are You a Founder, Angel, or Both?</label>
+							  <select className="form-control" id="role" value={this.state.role} onChange={this.handleInputChange}>
+							  	<option></option>
+							  	<option>Founder</option>
+							  	<option>Angel</option>
+							  	<option>Both</option>	    
+							  </select>
 							</div>
-							<button type="submit" className="btn btn-warning">Register</button>
+							<button type="submit" className="btn btn-warning" onClick={this.handleFormSubmit}>Register</button>
 						</form>
 					</div>
 				</div>
