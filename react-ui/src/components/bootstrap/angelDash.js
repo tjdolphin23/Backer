5869fd067a403c849593c846481bffd4a6a787ed
Benjamin Rodriguez <bimatn@outlook.com>
2018-01-01 16:36:29 -0700

    trying hybrid dashboard
    using bootstrap and css

--- react-ui/src/components/bootstrap/angelDash.js
+++ react-ui/src/components/bootstrap/angelDash.js
@@ -2,66 +2,13 @@ import React from 'react';
 
 const AngelDash = () =>
 
-	<div className="row">
-		<div className="col-12">
-			<div className="row justify-content-center mx-auto mt-4">
-				<div className="col-3">
-					<div className="card">
-					  <div className="card-body">
-					    <h5 className="card-title">My Projects</h5>
-					    <p className="card-text">Go Here To See What You've Built</p>
-					    <a href="#" className="btn btn-primary">My Projects</a>
-					  </div>
-					</div>
-				</div>
-				<div className="col-3">
-					<div className="card">
-					  <div className="card-body">
-					    <h5 className="card-title">My Projects</h5>
-					    <p className="card-text">Go Here To See What You've Built</p>
-					    <a href="#" className="btn btn-primary">My Projects</a>
-					  </div>
-					</div>
-				</div>
-				<div className="col-3">
-					<div className="card">
-					  <div className="card-body">
-					    <h5 className="card-title">My Projects</h5>
-					    <p className="card-text">Go Here To See What You've Built</p>
-					    <a href="#" className="btn btn-primary">My Projects</a>
-					  </div>
-					</div>
-				</div>
-			</div>
-			<div className="row justify-content-center mx-auto mt-4">
-				<div className="col-3">
-					<div className="card">
-					  <div className="card-body">
-					    <h5 className="card-title">My Projects</h5>
-					    <p className="card-text">Go Here To See What You've Built</p>
-					    <a href="#" className="btn btn-primary">My Projects</a>
-					  </div>
-					</div>
-				</div>
-				<div className="col-3">
-					<div className="card">
-					  <div className="card-body">
-					    <h5 className="card-title">My Projects</h5>
-					    <p className="card-text">Go Here To See What You've Built</p>
-					    <a href="#" className="btn btn-primary">My Projects</a>
-					  </div>
-					</div>
-				</div>
-				<div className="col-3">
-					<div className="card">
-					  <div className="card-body">
-					    <h5 className="card-title">My Projects</h5>
-					    <p className="card-text">Go Here To See What You've Built</p>
-					    <a href="#" className="btn btn-primary">My Projects</a>
-					  </div>
-					</div>
-				</div>
-			</div>
+	<div className="box">
+		<div className="card">
+		  <div className="card-body">
+		    <h5 className="card-title">My Projects</h5>
+		    <p className="card-text">Go Here To See What You've Built</p>
+		    <a href="#" className="btn btn-primary">My Projects</a>
+		  </div>
 		</div>
 	</div>
 
--- react-ui/src/components/bootstrap/founderDash.js
+++ react-ui/src/components/bootstrap/founderDash.js
@@ -3,7 +3,13 @@ import React from 'react';
 const FounderDash = ({css}) =>
 	
 	<div className="box">
-		<h1>TEST</h1>
+		<div className="card">
+		  <div className="card-body">
+		    <h5 className="card-title">My Projects</h5>
+		    <p className="card-text">Go Here To See What You've Built</p>
+		    <a href="#" className="btn btn-primary">My Projects</a>
+		  </div>
+		</div>
 	</div>
 
 export default FounderDash;
