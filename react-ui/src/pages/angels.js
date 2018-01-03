09cdfc88919e5208b45b6cfe7f771f06117a500c
Cumorah Masters <cumorahm@gmail.com>
2018-01-02 20:51:13 -0700

    Auto stash before merge of "master" and "origin/master"
    my work on the founder and angel pages

--- react-ui/src/pages/angels.js
+++ react-ui/src/pages/angels.js
@@ -6,16 +6,49 @@ import Testimonials from '../components/bootstrap/testimonials.js';
 import GoFish from '../components/bootstrap/title.js';
 import Jumbotron from '../components/bootstrap/jumbotronFluid.js';
 import Foot from '../components/bootstrap/foot.js';
+import JumbotronFluid from '../components/bootstrap/jumbotronFluid.js';
+import Title from '../components/bootstrap/title.js';
 
 const Home = () =>
 
 <div className="App">
-	<Jumbotron>
-		    <NavBar/>
-		    <GoFish/>
-	</Jumbotron>
+	<JumbotronFluid css="head">
+		<ContainerFluid>
+			    <NavBar/>
+			    <Title text1="angels" text2=""/>
+		</ContainerFluid>
+	</JumbotronFluid>
 	<Container>
-		<Testimonials/>
+		<div className="row">
+			<div className="col-12" className="colTitle">
+				<h1></h1>
+				<h1></h1>
+				<h1></h1>
+				<h2> How it works </h2>
+			</div>
+		</div>
+			<div className="row">
+			  <div className="Para1 col-8">
+					<p> By signing up with a free account you have access to thousands of products that are just waiting to take off. 
+					When signing up you will be asked questions about your interests and field of work to help set you up with
+					the best products to invest in. After finding the perfect investing opportunity, your project will have its own dashboard 
+					that you can connect with your investee and update your progress in real time.</p>
+			  </div>
+		    </div>
+			<div className="row">	
+				<div className="Para2">		
+					<h2 className="colTitle"> Benefits </h2>
+						<ul>
+						   <li> Get connected to thousands of products looking for business partners </li>
+						   <li> Access to a live dashboard to track your success! </li>
+						   <li> See your investee's product and brand grow </li>
+						   <li> Become a successful investor (financially & see your portfolio grow) </li>
+						</ul>
+				</div>
+				</div>
+			<div className="register">	
+				<button type="button" className="btn btn-default"> Register Now </button>
+			</div>
 	</Container>
 	<Foot/>
 </div>
--- react-ui/src/pages/founders.js
+++ react-ui/src/pages/founders.js
@@ -13,10 +13,37 @@ const Founders = () =>
 	<JumbotronFluid css="head">
 		<ContainerFluid>
 			    <NavBar/>
-			    <Title text1="FOUNDERS" text2="This is where more info can go"/>
+			    <Title text1="founders" text2=""/>
 		</ContainerFluid>
 	</JumbotronFluid>
 	<Container>
+		<div className="row">
+			<div className="col-12" className="colTitle">
+				<h1></h1>
+				<h2> How it works </h2>
+			</div>
+		</div>
+			<div className="row">
+			  <div className="Para1 col-8">
+					<p> By signing up with a free account you have access to thousands of investors that are looking to help you succeed in your business. 
+					When signing up you will be asked questions about the project you are on working to get you connected with the right 
+				     type of investor. From there, your project will have its own dashboard that you can connect with your investor and update your progress in real time.</p>
+			  </div>
+		    </div>
+			<div className="row">	
+				<div className="Para2">		
+					<h2 className="colTitle"> Benefits </h2>
+						<ul>
+						   <li> Get connected to thousands of Angels </li>
+						   <li> Access to a live dashboard to track your success! </li>
+						   <li> See your product and your brand grow </li>
+						   <li> Become a successful entrepreneur </li>
+						</ul>
+				</div>
+				</div>
+			<div className="register">	
+				<button type="button" className="btn btn-default"> Register Now </button>
+			</div>
 	</Container>
 	<Foot/>
 </div>
