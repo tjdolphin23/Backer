00b4ad26da08baee3ae5c1c4ae3b4d7f437a00e8
Benjamin Rodriguez <bimatn@outlook.com>
2018-01-04 10:51:41 -0700

    Modified title of testimonials
    More easily supports responsiveness

--- react-ui/src/components/bootstrap/testimonials.js
+++ react-ui/src/components/bootstrap/testimonials.js
@@ -5,14 +5,20 @@ const Testimonials = () =>
 		<div className="col-12 col-md-4 mt-2">
 			<div className="card testiCard">
 			  <div className="card-header testiColor">
-					<div className="row justify-content-center">
-						<div className="col-4 testiTitle1"><h5>Speak It</h5></div>
-						<div className="col-4 testiTitle2"><h5>Darcy P.</h5>Gilbert, AZ</div>
-						<div className="col-4 testiPicDiv">
-						<img src="./assets/images/darcy.jpeg" alt="darcy" className="img-thumbnail"></img>
+					<div className="row">
+						<div className="col-7 text-center">
+							<div className="row">
+								<div className="col-12 testiTitle">Speak It</div>
+							</div>
+							<div className="row">
+								<div className="col-12 testiTitle">Darcy P. Gilbert, AZ</div>
+							</div>
+						</div>
+						<div className="col-5 text-center">
+							<img src="./assets/images/darcy.jpeg" alt="darcy" className="img-thumbnail"></img>
+						</div>
 					</div>
 				</div>
-				</div>	
 			  <div className="card-body">
 			    <p className="card-text">"I can't believe how helpful Angel Fish was in connecting me with intelligent business professionals"</p>
 			    <a href="/wip" alt="testimonial" className="testiColor">Learn More about Darcy's Experience</a>
@@ -22,11 +28,17 @@ const Testimonials = () =>
 		<div className="col-12 col-md-4 mt-2">
 			<div className="card testiCard">
 			  <div className="card-header testiColor">
-					<div className="row justify-content-center">
-						<div className="col-4 testiTitle1"><h5>BroGainz</h5></div>
-						<div className="col-4 testiTitle2"><h5>Alex A.</h5>Phoenix, AZ</div>
-						<div className="col-4 testiPicDiv">
-								<img src="./assets/images/alex.jpeg" alt="alex" className="img-thumbnail"></img>
+					<div className="row">
+						<div className="col-7 text-center">
+							<div className="row">
+								<div className="col-12 testiTitle">BroGainz</div>
+							</div>
+							<div className="row">
+								<div className="col-12 testiTitle">Alex A. Phoenix, AZ</div>
+							</div>
+						</div>
+						<div className="col-5 text-center">
+							<img src="./assets/images/alex.jpeg" alt="Alex" className="img-thumbnail"></img>
 						</div>
 					</div>
 				</div>	
@@ -39,11 +51,17 @@ const Testimonials = () =>
 		<div className="col-12 col-md-4 mt-2">
 			<div className="card testiCard">
 			  <div className="card-header testiColor">
-					<div className="row justify-content-center">
-						<div className="col-4 testiTitle1"><h5>Codopoly</h5></div>
-						<div className="col-4 testiTitle2"><h5>Carson H.</h5>Scottsdale, AZ</div>
-						<div className="col-4 testiPicDiv">
-								<img src="./assets/images/carson.jpeg" alt="carson" className="img-thumbnail"></img>
+					<div className="row">
+						<div className="col-7 text-center">
+							<div className="row">
+								<div className="col-12 testiTitle">Codopoly</div>
+							</div>
+							<div className="row">
+								<div className="col-12 testiTitle">Carson H. Scottsdale, AZ</div>
+							</div>
+						</div>
+						<div className="col-5 text-center">
+							<img src="./assets/images/carson.jpeg" alt="Carson" className="img-thumbnail"></img>
 						</div>
 					</div>
 				</div>	
