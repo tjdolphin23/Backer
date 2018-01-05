a032913c1a9b3b403f0d66bfd02cbad7d2a5910d
Benjamin Rodriguez <bimatn@outlook.com>
2018-01-05 13:41:08 -0700

    Added options to user menu

--- react-ui/src/components/bootstrap/dashNav.js
+++ react-ui/src/components/bootstrap/dashNav.js
@@ -5,32 +5,43 @@ class DashNav extends Component {
 	handleSignOut = () => {
 		sessionStorage.removeItem("userName");
 		sessionStorage.removeItem("userId");
+		window.location = ("/");
 	}
 
 	render () {
 		return (
 			<nav className="navbar navbar-expand-lg navbar-light">
 				<span className="navbar-brand">
-					<a href="/"><img src="assets/images/angelFish.png" width="30" height="30" className="d-inline-block align-top" alt="angelFish logo"></img></a> angel fish
+					<a href="/"><img src="assets/images/angelFish.png" width="30" height="40" className="d-inline-block align-top" alt="angelFish logo"></img></a> angel fish
 				</span>
 				<button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 				  <span className="navbar-toggler-icon"></span>
 				</button>
 				<div className="collapse navbar-collapse" id="navbarNavDropdown">
-				  <ul className="navbar-nav ml-auto mr-4">
-				  	<li className="nav-item">
-				  		<img src="/open-iconic/svg/icon-name.svg" alt="icon name">
-				  	</li>
-				    <li className="nav-item dropdown">
-				      <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-				        Welcome, {sessionStorage.getItem("userName")}
-				      </a>
-				      <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-				        <a className="dropdown-item" href="/viewProfile">View Profile</a>
-				        <a className="dropdown-item" onClick={this.handleSignOut} href="/">Sign Out</a>
-				      </div>
-				    </li>
-				  </ul>
+					<ul className="navbar-nav ml-auto mr-4">
+						<li className="nav-item dropdown">
+						  <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+						    Welcome, {sessionStorage.getItem("userName")} <span className="oi oi-bell nav-link" title="alerts" aria-hidden="true"></span>
+						  </a>
+						  <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+						    <button className="dropdown-item">
+						    	<span className="oi oi-calendar nav-link" title="calendar" aria-hidden="true"></span> Events
+						    </button>
+						    <button className="dropdown-item">
+						    	<span className="oi oi-inbox nav-link" title="inbox" aria-hidden="true"></span> Messages
+						    </button>
+						    <button className="dropdown-item">
+						    	<span className="oi oi-chat nav-link" title="chat" aria-hidden="true"></span> Chat
+						    </button>
+						    <button className="dropdown-item">
+						    	<span className="oi oi-cog nav-link" title="settings" aria-hidden="true"></span> Settings
+						    </button>
+						    <button className="dropdown-item">
+						    	<span className="oi oi-account-logout nav-link" onClick={this.handleSignOut} title="sign out" aria-hidden="true"></span> Sign Out
+						    </button>
+						  </div>
+						</li>
+					</ul>
 				</div>
 			</nav>
 		)
