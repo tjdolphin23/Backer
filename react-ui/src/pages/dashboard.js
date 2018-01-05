969ba9cbbc2626672a95fd11a865e0474498f38b
Benjamin Rodriguez <bimatn@outlook.com>
2018-01-05 15:23:57 -0700

    Some progress on dashboard control

--- react-ui/src/pages/dashboard.js
+++ react-ui/src/pages/dashboard.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import DashNav from '../components/bootstrap/dashNav.js';
 import FounderDash from '../components/bootstrap/founderDash.js';
 import AngelDash from '../components/bootstrap/angelDash.js';
@@ -8,23 +8,52 @@ import Title from '../components/bootstrap/title.js';
 import JumbotronFluid from '../components/bootstrap/jumbotronFluid.js';
 import Foot from '../components/bootstrap/foot.js';
 
-const Dashboard = (props) =>
+class Dashboard extends Component {
 
-<div>
-	<DashNav/>
-	<ContainerFluid css="dashBox">
-		<div className="row">
-			<div className="col-6 rightBorder text-center">
-				<FounderDash/>
+	state = {
+		leftDisplay: "d-block",
+		rightDisplay: "d-block",
+		columns: "col-6",
+		colexLeft: "collapse-left",
+		colexRight: "collapse-right"
+	}
+
+	handleCollapseLeft = () => {
+	    this.setState ({
+	    	leftDisplay: "d-none",
+	    	columns: "col-12"
+	    })
+	}
+
+	handleCollapseRight = () => {
+		this.setState ({
+			rightDisplay: "d-none",
+			columns: "col-12"
+		})
+	}
+
+	render(){
+		return(
+			<div>
+				<DashNav/>
+				<ContainerFluid css="dashBox">
+					<div className="row">
+						<div className={`${this.state.columns} rightBorder text-center ${this.state.leftDisplay}`} id="fSection">
+							<FounderDash/>
+							<span className={`oi oi-${this.state.colexLeft} nav-link`} onClick={this.handleCollapseLeft} title="colex left" aria-hidden="true"></span>
+						</div>
+						<div className={`${this.state.columns} lefttBorder text-center ${this.state.rightDisplay}`} id="aSection">
+							<AngelDash/>
+							<span className={`oi oi-${this.state.colexRight} nav-link`} onClick={this.handleCollapseRight} title="colex right" aria-hidden="true"></span>
+						</div>
+					</div>
+				</ContainerFluid>
+				<ContainerFluid css="foot">
+					<Foot/>
+				</ContainerFluid>
 			</div>
-			<div className="col-6 leftBorder text-center">
-				<AngelDash/>
-			</div>
-		</div>
-	</ContainerFluid>
-	<ContainerFluid css="foot">
-		<Foot/>
-	</ContainerFluid>
-</div>
+		)
+	}
+}
 
 export default Dashboard;
