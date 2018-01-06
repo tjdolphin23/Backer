d00b1359b9816fea9002ca62a94f6953cf5dbefa
Cumorah Masters <cumorahm@gmail.com>
2018-01-06 13:49:02 -0700

    about us page

--- react-ui/src/pages/about.js
+++ react-ui/src/pages/about.js
@@ -7,6 +7,8 @@ import GoFish from '../components/bootstrap/title.js';
 import Jumbotron from '../components/bootstrap/jumbotronFluid.js';
 import Foot from '../components/bootstrap/foot.js';
 import Axios from 'axios';
+import JumbotronFluid from '../components/bootstrap/jumbotronFluid.js';
+import Title from '../components/bootstrap/title.js';
 
 class Home extends Component {
 	
@@ -18,14 +20,24 @@ class Home extends Component {
 	render () {
 		return (
 			<div>
-				<Jumbotron>
-					    <NavBar/>
-					    <GoFish/>
-				</Jumbotron>
+				<JumbotronFluid css="head">
+		          <ContainerFluid>
+			        <NavBar t1="Founders" l1="/founders" t2="Angels" l2="/angels" t3="About" l3="/about" t4="Start Now" l4="/start"/>
+			        <Title text1="about us" text2=""/>
+				  </ContainerFluid>
+				</JumbotronFluid>
 				<Container>
-					<Testimonials/>
+					<div>
+						<h2 className="aboutTitle"> How we got started </h2> 
+						<p className="about"> Angel Fish was founded in Gilbert, AZ Winter of 2017. From there the founders: Ben, Tom, Skylar, Cumorah, and Damien 
+						sought out a way to connect entrepreneurs and investors to make their dreams come true. Like the founders, many out there have 
+						ideas or products that just don't have the finanicial means in making their ideas a reality. </p> 
+					</div>
+
 				</Container>
-				<Foot/>
+				<ContainerFluid css="foot">
+					<Foot/>
+				</ContainerFluid>
 			</div>
 		);
 	};
