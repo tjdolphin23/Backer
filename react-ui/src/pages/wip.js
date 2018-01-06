93b3e1e9f08ad69a80a6ae2d0cf8eaf7de4038e2
Benjamin Rodriguez <bimatn@outlook.com>
2018-01-06 11:12:51 -0700

    added work in progress page

---
+++ react-ui/src/pages/wip.js
@@ -0,0 +1,40 @@
+import React, {Component} from 'react';
+import NavBar from '../components/bootstrap/navbar.js';
+import Container from '../components/bootstrap/container.js';
+import ContainerFluid from '../components/bootstrap/containerFluid.js';
+import Title from '../components/bootstrap/title.js';
+import JumbotronFluid from '../components/bootstrap/jumbotronFluid.js';
+import Foot from '../components/bootstrap/foot.js';
+
+class Start extends Component {
+
+	componentWillMount () {
+		console.log();
+		if(sessionStorage.getItem("userName") && sessionStorage.getItem("userId")){
+			window.location = ("/dashboard");
+		}
+	}
+
+	render() {
+		return (
+			<div>
+				<JumbotronFluid css="head">
+					<ContainerFluid>
+						    <NavBar t1="Founders" l1="/founders" t2="Angels" l2="/angels" t3="About" l3="/about" t4="Start Now" l4="/start"/>
+						    <Title text1="angel fish" text2="Work In Progress..."/>
+					</ContainerFluid>
+				</JumbotronFluid>
+				<Container>
+					<div className="row justify-content-center">
+						<p className="mt-5"><h1>Hey! We're Working Here!</h1></p>
+					</div>
+				</Container>
+				<ContainerFluid css="foot">
+					<Foot/>
+				</ContainerFluid>
+			</div>
+		)
+	}
+}
+
+export default Start;
