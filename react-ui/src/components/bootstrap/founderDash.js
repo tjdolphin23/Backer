0c804864c23300834063c618a2f25262339d1fbf
Benjamin Rodriguez <bimatn@outlook.com>
2018-01-08 11:19:10 -0700

    developing functionality of founder dash

--- react-ui/src/components/bootstrap/founderDash.js
+++ react-ui/src/components/bootstrap/founderDash.js
@@ -1,15 +1,52 @@
-import React from 'react';
+import React, {Component} from 'react';
+import ProjectForm from './projectForm.js'
 
-const FounderDash = () =>
-	
-	<div className="innerDashBox">
-		<div className="card">
-		  <div className="card-body">
-		    <h5 className="card-title">My Projects</h5>
-		    <p className="card-text">Go Here To See What You've Built</p>
-		    <a href="#" className="btn btn-primary">My Projects</a>
-		  </div>
-		</div>
-	</div>
+class FounderDash extends Component {
+
+	state = {
+		boxVisibility: "",
+		projectFormVisibility: "d-none"
+	}
+
+	handleProjectsClick = e => {
+		e.preventDefault();
+		this.setState({
+			boxVisibility: "d-none",
+			projectFormVisibility: ""
+		})
+	}
+
+	handleCreateClick = e => {
+		e.preventDefault();
+		this.setState({
+			boxVisibility: "d-none",
+			projectFormVisibility: ""
+		})
+	}
+
+	render() {
+		return (
+			<div>
+				<div className={`innerDashBox ${this.state.boxVisibility}`}>
+					<div className="card col-4 mx-auto">
+					  <div className="card-body">
+					    <h5 className="card-title">Create Project</h5>
+					    <a href="#" className="btn btn-primary" onClick={this.handleCreateClick}>Create</a>
+					  </div>
+					</div>
+					<div className="card col-4 mx-auto">
+					  <div className="card-body">
+					    <h5 className="card-title">View Projects</h5>
+					    <a href="#" className="btn btn-primary" onClick={this.handleProjectsClick}>Projects</a>
+					  </div>
+					</div>
+				</div>
+				<div className={this.state.projectFormVisibility}>
+					<ProjectForm/>
+				</div>
+			</div>
+		)
+	}
+}
 
 export default FounderDash;
