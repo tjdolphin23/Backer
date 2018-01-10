f0e9959c467c1d4168c8e7efffc372f414c38db8
Benjamin Rodriguez <bimatn@outlook.com>
2018-01-10 14:17:33 -0700

    search bar functionality progress
    need to target correct correct api endpoint
    need to work on 'this'

--- react-ui/src/components/dashboards/angelDash.js
+++ react-ui/src/components/dashboards/angelDash.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import Axios from 'axios';
 import InnerDashBox from '../innerDashBox.js';
 import DashCard from '../cards/dashCard.js';
 import SearchBar from '../search/searchBar.js';
@@ -7,31 +8,53 @@ class AngelDash extends Component {
 
 	constructor (props) {
 		super(props);
-		this.visibleButtonsHandler = this.visibleButtonsHandler.bind(this);
+		this.buttonsVisibleHandler = this.buttonsVisibleHandler.bind(this);
 		this.state = {
 			buttonsVisible: "d-block",
 			searchVisible: "d-none"
 		}
 	}
 
-	visibleButtonsHandler = () => {
+	buttonsVisibleHandler = () => {
 		this.setState(
-			{buttonsVisible: "d-none"}
+			{
+				buttonsVisible: "d-none",
+				searchVisible: "d-block"
+			}
 		);
+	}
+
+	axiosSearch = () => {
+		const {search} = this.state;
+		console.log(search);
+		//Axios Request
+	  	Axios({
+	  	  method: 'post',
+	  	  url: '/api/angels/productSearch',
+	  	  data: {
+	  	    "search": search
+	  	  }
+	  	}).then(function (response) {
+	  		console.log(response)
+	  	  })
+	  	  .catch(function (error) {
+	  	    console.log(error);
+	  	    alert("There seems to be a problem. Please try again.");
+	  	  });
 	}
 
 	render () {
 		return (
 			<div>
-				<div className={this.state.buttonVisible}>
+				<div className={this.state.buttonsVisible}>
 					<InnerDashBox>
-						<DashCard title="Find Products" button="Search" clickAction={this.visibleButtonsHandler}/>
-						<DashCard title="View Investments" button="Investments" clickAction={this.visibleButtonsHandler}/>
+						<DashCard title="Find Products" button="Search" clickAction={this.buttonsVisibleHandler}/>
+						<DashCard title="View Investments" button="Investments" clickAction={this.buttonsVisibleHandler}/>
 					</InnerDashBox>
 				</div>
 				<div className={this.state.searchVisible}>
 					<InnerDashBox>
-						<SearchBar/>	
+						<SearchBar clickAction={this.axiosSearch}/>	
 					</InnerDashBox>
 				</div>
 			</div>
--- react-ui/src/components/search/searchBar.js
+++ react-ui/src/components/search/searchBar.js
@@ -5,9 +5,16 @@ class SearchBar extends Component {
 	constructor (props) {
 		super(props);
 		this.state = {
-
-		}
+			search: ""
+		};
 	}
+
+	changeHandler = event => {
+		const {id, value} = event.target;
+		this.setState ({
+			[id]:value
+		});
+	};
 
 	handleClick = (e) => {
 		e.preventDefault();
@@ -16,12 +23,12 @@ class SearchBar extends Component {
 		});
 	}
 
-	render() {
+	render(props) {
 		return(
 			<div className="input-group mb-3">
-			  <input type="text" className="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="basic-addon2"></input>
+			  <input type="text" className="form-control" id="search" placeholder="Find a product..." aria-label="Recipient's username" aria-describedby="basic-addon2" value={this.state.search} onChange={this.changeHandler}></input>
 			  <div className="input-group-append">
-			    <button className="btn btn-outline-secondary" type="button">Button</button>
+			    <button className="btn btn-secondary" type="button" onClick={this.props.clickAction}>Search</button>
 			  </div>
 			</div>
 		)
